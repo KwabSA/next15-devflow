3a4f361c5709c48089349e60063e2cbd0fc31ad4
KwabSA <141826847+KwabSA@users.noreply.github.com>
2024-11-10 16:40:49 +0000

    Eslinst and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 const Home = () => (
-  <h1 className="text-3xl text-white font-black">Welcome to DevFlow</h1>
+  <h1 className="text-3xl font-black text-white">Welcome to DevFlow</h1>
 );
 
 export default Home;
